513739c9a4464bc6c23b05c4c19e772733a7f0f3
sochoag <santiagodav@hotmail.es>
2025-05-17 22:05:51 -0500

    feat(api): :lock: Adding basic auth to all API
    Now all the requests needs username and password

--- app/src/main.py
+++ app/src/main.py
@@ -3,8 +3,35 @@ from .api.endpoints import router as api_router
 from .avro.endpoints import router as backup_router
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette.status import HTTP_401_UNAUTHORIZED
+import secrets
 import os
+
+security = HTTPBasic()
+
+USERS = {
+    os.getenv("API_USER"): os.getenv("API_PASS")
+}
+
+
+def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
+    if credentials.username not in USERS:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    if not secrets.compare_digest(credentials.password, USERS[credentials.username]):
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
 
 # Create data folder if it doesn't exist
 if not os.path.exists("./.data"):
@@ -13,8 +40,8 @@ if not os.path.exists("./.data"):
 app = FastAPI()
 
 # Routers
-app.include_router(api_router)
-app.include_router(backup_router)
+app.include_router(api_router, dependencies=[Depends(authenticate_user)])
+app.include_router(backup_router, dependencies=[Depends(authenticate_user)])
 
 
 @app.exception_handler(RequestValidationError)
